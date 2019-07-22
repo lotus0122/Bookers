e9abffa0e1957b9153037c8ff860c78484ae5b5b
lotus0122 <ddd301no1@gmail.com>
2019-07-22 18:08:37 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'posts#top'
+  root:to => 'books#top'
+  
+  resources :books
 
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
+
+  delete 'posts/:id' => 'posts#destroy' , as:'destroy_post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
-  resources :books
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 2019_07_15_101457) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
